7814d26487711a51cbadc1191ac2976a1054a223
Debasis kar <debasiskar007@gmail.com>
2019-07-10 17:04:44 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MetaGuard } from '@ngx-meta/core';
 
-const routes: Routes = [];
+
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+
+const appRoutes: Routes = [
+  { path: 'about', component: AboutusComponent,data: {
+      meta: {
+          title: 'About US',
+          description: 'Have you seen my rubber duckie?'
+      }
+  } },
+  { path: 'contact',      component: ContactusComponent,
+      data: {
+          meta: {
+              title: 'Contact Us',
+              description: 'Have you seen my rubber duckie?'
+          }
+      }}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { MetaModule } from '@ngx-meta/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutusComponent,
+    ContactusComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,MetaModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
